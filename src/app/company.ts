f7eb7e3b43c451a80c39f1d82f5dc48cbeab6f47
bwyard <45057171+bwyard@users.noreply.github.com>
2020-04-12 23:58:15 -0500

    updated companyCore

--- src/app/company.ts
+++ src/app/company.ts
@@ -1,30 +1,31 @@
-export let company:{
-  name: string,
-  cash: number,
-  inventoryMax: number,
-  travelSpeed: number,
-  landTravel: boolean,
-  flightTravel: boolean,
-  seaTravel: boolean,
-  currentLocation: object,
-  inventory: number[],
-  inventoryTotal():number ():{}
-} = {
-  name: 'Brent',
-  cash: 50000,
-  inventoryMax:50,
-  travelSpeed:1,
-  landTravel:true,
-  flightTravel:false,
-  seaTravel:true,
-  currentLocation: null,
-  inventory: [0,0,0,0],
+export class Company{
+  name: string;
+  cash: number;
+  inventoryMax: number;
+  travelSpeed: number;
+  landTravel: boolean;
+  flightTravel: boolean;
+  seaTravel: boolean;
+  currentLocation: object;
+  inventory: number[];
+  constructor(name:string,cash:number,inventoryMax:number,travelSpeed:number,landTravel:boolean,flightTravel:boolean,seaTravel:boolean,currentLocation:object,inventory:number[]){
+    this.name = name;
+    this.cash = cash;
+    this.inventoryMax = inventoryMax;
+    this.travelSpeed = travelSpeed;
+    this.landTravel = landTravel;
+    this.flightTravel = flightTravel;
+    this.seaTravel = seaTravel;
+    this.currentLocation = currentLocation;
+    this.inventory = inventory;
+  }
   inventoryTotal():number {
     let totalInv:number = 0;
     for (let i of this.inventory){
-      totalInv =+ i;
+      totalInv += Number(i);
+      console.log(`inventory total being counter:${i}`);
 
     }
     return totalInv;
   }
-};
+}
---
+++ src/app/companyCore.ts
@@ -0,0 +1,4 @@
+import { Company } from "./company";
+
+export const COMPANY: Company =
+  new Company('Brent',50000, 50,1,true,false,true,null, [0, 0, 0, 0], )
